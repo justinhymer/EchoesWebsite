7d2b817fc1bce633fc48c3041dc0e93c2f343fad
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2023-10-23 15:57:46 -0500

    email first commit
    - email with nodemailer
    - added POST route per Nodemailer docs/examples
    - need to build front end

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 require("dotenv").config();
+const bodyParser = require('body-parser'); // run npm i body-parser
+const nodemailer = require('nodemailer'); // run npm i nodemailer
 
 const app = express();
 const router = express.Router();
 const PORT = process.env.PORT || 3001;
 const path = require("path");
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// NodeMailer
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+port: 465,               // true for 465, false for other ports
+host: "smtp.gmail.com", // change to something more generic (We only need users to send emails to Hymer)
+   auth: {
+        user: 'youremail@gmail.com',
+        pass: 'password',
+     },
+secure: true,
+});
 
 // THE SERVER
 app.use(express.static(path.join(__dirname, "/")));
@@ -15,5 +29,29 @@ app.use(express.static(path.join(__dirname, "/")));
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./index.html"));
 });
+
+// EMAIL HANDLING (https://nodemailer.com/)
+router.post("/send", (req, res) => {
+  const {from, subject, text} = req.body;
+
+  // write function to format text to HTML
+  // text.for() {} -- use to perform formatting and assign to var named "formatted"
+  // let textHTML = formatted
+  
+  const mailData = {
+  from: from,  // sender address
+  to: 'myfriend@gmail.com',   // Hymer's email address (MOODY's for testing)
+  subject: subject,
+  text: text,
+  html: formatted,
+
+  transporter.sendMail(mailOptions, function (err, info) {
+   if(err)
+     console.log(err)
+   else
+     console.log(info);
+});
+};
+})
 
 app.listen(PORT, () => console.log(`App is live on port ${PORT}!`));
